999cf089d94f090b5418b0e3bdd3206319df3c6d
Rosybliz <bchoco961@gmail.com>
2023-07-14 09:58:55 -0700

    My commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>	
+/**
+ * man - prints lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'z
+		'
+		while( x >= 'a')
+		{
+			putchar(x);
+			z--;
+		}
+	putchar('\n');
+	return(0);
+}
