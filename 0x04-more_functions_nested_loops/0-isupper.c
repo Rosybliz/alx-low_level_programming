6311b4d9df38aac1126c4c63d20f9632e44b5f01
Rosybliz <bchoco961@gmail.com>
2023-07-20 08:28:40 -0700

    My commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - a function that prints uppercase
+ * Return 1 if uppercase or 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+
+	if(c>=65 && c<=90);
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
