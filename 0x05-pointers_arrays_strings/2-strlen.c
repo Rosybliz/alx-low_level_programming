9f7251304138984f8ed301d34df6c160b477a45f
Rosybliz <bchoco961@gmail.com>
2023-07-24 07:49:26 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * int _strlen - a function that reyurns the length of a string
+ * @s: the string variable
+ * @len: the variable that holds the string length
+ *
+ * Return : len
+ */
+
+int _strlen(char *s)
+{
+len = _strlen(s);
+
+return (len);
+}
