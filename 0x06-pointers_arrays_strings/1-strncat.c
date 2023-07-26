6831bab0d93ef97d5ceaa397ce408b3be7838e5b
Rosybliz <bchoco961@gmail.com>
2023-07-26 10:22:07 -0700

    My commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncat - joins or concatenates two strings
+ * @dest: string one
+ * @src: string to be appended
+ * Description: two strings are joined and a terminating
+ * null byte added
+ * Return : dest
+ */
+
+char *_strncat(char *dest, char *src)
+{
+int i = 0;
+int index = 'n';
+while (dest[i] != '\0')
+{
+++i;
+}
+for (i = 0; src[i] <= index; ++i)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
