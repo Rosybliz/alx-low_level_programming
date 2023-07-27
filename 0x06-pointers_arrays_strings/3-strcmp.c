acdc4d34f40f163e0323f08ef7e4a230396784a7
Rosybliz <bchoco961@gmail.com>
2023-07-27 05:19:37 -0700

    My commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,3 +2,31 @@
 
 /**
  * _strcmp - compares two strings
+ * @s1:  first string for comparison
+ * @s2: second string for comparison
+ * Return: return 0 if equal,
+ * >0 if s1 is greater and <0 if otherwise
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1 == s2)
+		{
+			++s1;
+			++s2;
+			return (0);
+		}
+		else if (s1 > s2)
+		{
+			return (s1 > 0);
+		}
+		else
+		{
+			return (s1 < 0);
+		}
+}
+
