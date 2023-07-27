7b5776b3aefe6b18b72ce367f518b6bc0eee4fcf
Rosybliz <bchoco961@gmail.com>
2023-07-27 04:40:30 -0700

    My commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,13 +10,15 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int n = 0;
-	while( *src != '\0')
-	{
-		*dest = *src;
-		++src;
-		++dest;
-		*dest = '\0';
-	}
+	int i;
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
 }
