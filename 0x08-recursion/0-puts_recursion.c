2752562486ca61a33a035db606c7bd43f80b1c31
Rosybliz <bchoco961@gmail.com>
2023-08-02 07:13:05 -0700

    My commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,24 +3,20 @@
 /**
  * _puts_recursion - the recursive function
  * @s: the string to be printed
- * @n: the index of the strings
- * @a: the variable that holds the string outputs
  * Return: always 0
  */
 
 void _puts_recursion(char *s)
 {
-	int n = 0;
-	int a;
-
-	if (*s == '\0')
-		return (s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 	else
-
-		a = _puts_recursion(*s[n + 1]);
-		_putchar(a);
-
-	_putchar('\n');
+	{
+		_putchar('\n');
+	}
 }
 
 
