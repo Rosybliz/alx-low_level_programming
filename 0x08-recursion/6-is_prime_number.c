112bc3565ad1cb1007ec8efe4bf2619d3dbef660
Rosybliz <bchoco961@gmail.com>
2023-08-02 12:52:27 -0700

    My commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number(int n)- a recursive function that outputs the
+ * result of prime numbers
+ * @n: the integer
+ * Return: returns 1 if integer is a prime number 0 if otherwise
+ */
+
+int is_prime_number(int n)
+{
+	if ((n / n) == 1)
+		return (1);
+	if ((n / n) != 1)
+		return (0);
+	return (n / (is_prime_number(n - 1)));
+}
