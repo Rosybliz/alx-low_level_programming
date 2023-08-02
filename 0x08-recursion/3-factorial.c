3be0fb004582aec2aafb0f8cf9a0fdf6b963e6a1
Rosybliz <bchoco961@gmail.com>
2023-08-02 09:55:55 -0700

    My commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial(int n)- a recursive function that returns the
+ * factorial of a given number
+ * @n: the given number
+ * Return: return -1 if n is lower than 0 to indicate error
+ */
+
+int factorial(int n)
+{
+	int s = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		s = n * factorial(n - 1);
+		return (s);
+	}
+}
