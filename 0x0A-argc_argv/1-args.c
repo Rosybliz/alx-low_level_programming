e7d44f440c597356f7138190eaba5bfdbe1b03a2
Rosybliz <bchoco961@gmail.com>
2023-08-04 09:58:32 -0700

    My commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- a program that prints the number
+ * of arguments passed to it
+ * @argc: the count of arguments supplied
+ * @argv: the array of pointer to the string
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
