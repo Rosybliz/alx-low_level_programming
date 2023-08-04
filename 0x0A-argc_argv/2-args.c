1835f41accb69366e28dc15fa0b362d9e8ed5df7
Rosybliz <bchoco961@gmail.com>
2023-08-04 11:06:13 -0700

    My commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- a program that prints all arguments it receives
+ * @argc: the count of arguments
+ * @argv: the pointer to the argument strings
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 1; i <= 1; ++i)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
