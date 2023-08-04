f90510c65688b2e62141897efeea237437429f65
Rosybliz <bchoco961@gmail.com>
2023-08-04 09:23:22 -0700

    My cmmit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include "main.h"
 
-int main(int argc, char *argv[])
+/**
+ * main- a program that prints its name
+ * followed by a new line
+ * @argc: the argument count of type integer
+ * @argv: the array of strings that hold the
+ * command line arguments
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 		return (0);
 }
