313e0fa7368b004d150a321c6d047a6348e33bd2
Rosybliz <bchoco961@gmail.com>
2023-08-14 07:23:58 -0700

    My commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* struct dog- a structure with three members
+* @name: first member
+* @age: second member
+* @owner: third member
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
