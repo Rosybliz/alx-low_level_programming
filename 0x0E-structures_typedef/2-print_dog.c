d308aff680ba39b5f9e8bc428d6f0a7dc9bb9af7
Rosybliz <bchoco961@gmail.com>
2023-08-14 09:55:36 -0700

    My commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * print_dog- a function that prints a struct dog structure
+ * @d: the object of the structure
+ *
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+
+	if ((*d).name == NULL)
+		printf("\n%s: (nil)", (*d).name);
+	else if ((*d).age == 0.000000)
+		printf("\n%f: (nil)", (*d).age);
+	else if ((*d).owner == NULL)
+		printf("\n%s: (nil)", (*d).owner);
+	else if (d == NULL)
+		printf(" ");
+	else
+		printf("Name: %s\n Age: %f\n Owner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
